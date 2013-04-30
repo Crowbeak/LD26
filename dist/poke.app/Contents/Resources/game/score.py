c56a1039aaeb4635f75ab1fc4219ecfd59e2b2a6
Crowbeak <crowbeak@gmail.com>
2013-04-30 15:35:29 +0900

    Trying to make git ignore build files

--- dist/poke.app/Contents/Resources/game/score.py
+++
@@ -1,6 +0,0 @@
-import pyglet
-
-class Score(object):
-    def __init__(self):
-        self.eyes_poked = 0
-        self.timer = 2500
--- dist/poke.app/Contents/Resources/game/util.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def distance(point_1=(0, 0), point_2=(400, 300)):
-    return math.sqrt((point_1[0]-point_2[0])**2 + (point_1[1]-point_2[1])**2)
